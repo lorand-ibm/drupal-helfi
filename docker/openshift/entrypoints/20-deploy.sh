81dad4b37e4eca696be86f800baa60d8adec249f
SuviVappula <79191769+SuviVappula@users.noreply.github.com>
2022-01-13 15:25:59 +0200

    Update 20-deploy.sh

--- docker/openshift/entrypoints/20-deploy.sh
+++ docker/openshift/entrypoints/20-deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 cd /var/www/html/public && drush deploy
-cd /var/www/html/public && drush deploy
-cd /var/www/html/public && drush deploy
-cd /var/www/html/public && drush deploy
-cd /var/www/html/public && drush deploy
